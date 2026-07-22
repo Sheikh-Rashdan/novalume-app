1b3c7b4af173f352785dc79d9b4473a50801da5a
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-07-22 18:48:50 +0530

    Edited appliance model to make powerKwh mutable

--- lib/models/appliance.dart
+++ lib/models/appliance.dart
@@ -3,7 +3,7 @@ import 'package:uuid/uuid.dart';
 class Appliance {
   late String id;
   final String name;
-  final double powerKwh;
+  double powerKwh;
 
   Appliance({String? id, required this.name, required this.powerKwh})
     : assert(powerKwh >= 0),
