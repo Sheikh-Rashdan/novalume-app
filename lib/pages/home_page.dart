57180ac67a3d7dee5f7fb10e0d2f2565c57bbfa7
Sheikh-Rashdan <sheikhmohamedrashdan@gmail.com>
2026-07-23 21:10:55 +0530

    Moved the method call for testProviderForBubbleChart into the provider constructor

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -15,7 +15,6 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    context.read<ApplianceProvider>().testProviderForBubbleChart();
     return Scaffold(
       appBar: AppBar(
         title: GestureDetector(
